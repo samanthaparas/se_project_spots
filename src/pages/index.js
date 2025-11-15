2400681403c107d55d06218d95050e8618fb55bd
Samantha P <samantha.paras94@gmail.com>
2025-11-15 12:08:34 -0600

    API Endpoints

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,8 +65,8 @@ const api = new Api({
 });
 
 api
-  .getInitialCards()
-  .then((cards) => {
+  .getAppInfo()
+  .then(([cards]) => {
     cards.forEach((item) => {
       const cardElement = getCardElement(item);
       cardsList.append(cardElement);
